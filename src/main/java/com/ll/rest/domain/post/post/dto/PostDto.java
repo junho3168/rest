1983b237b56ac4efea78ae7223d74913f79116be
junho3168 <junho3168@naver.com>
2025-01-07 12:14:28 +0900

    @JsonIgnore

--- src/main/java/com/ll/rest/domain/post/post/dto/PostDto.java
+++ src/main/java/com/ll/rest/domain/post/post/dto/PostDto.java
@@ -1,5 +1,6 @@
 package com.ll.rest.domain.post.post.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.ll.rest.domain.post.post.entity.Post;
 import com.ll.rest.global.jpa.entity.BaseTime;
 import jakarta.persistence.Column;
@@ -11,17 +12,26 @@ import java.time.LocalDateTime;
 @Getter
 public class PostDto {
     private long id;
-    private LocalDateTime createdDateTime;
-    private LocalDateTime modifiedDateTime;
+    @JsonIgnore
+    private LocalDateTime createDate;
+    @JsonIgnore
+    private LocalDateTime modifyDate;
     private String title;
     private String content;
 
     public PostDto(Post post) {
         this.id = post.getId();
-        this.createdDateTime = post.getCreatedAt();
-        this.modifiedDateTime = post.getModifiedAt();
+        this.createDate = post.getCreatedAt();
+        this.modifyDate = post.getModifiedAt();
         this.title = post.getTitle();
         this.content = post.getContent();
 
+    }
+    public LocalDateTime getCreatedDatetime(){
+        return createDate;
+    }
+
+    public LocalDateTime getModifiedDatetime() {
+        return modifyDate;
     }
 }
