53076da5b6f2e71198070c2496a706133671edd9
junho3168 <junho3168@naver.com>
2025-01-07 12:08:19 +0900

    Dto name changed

--- src/main/java/com/ll/rest/domain/post/post/dto/PostDto.java
+++ src/main/java/com/ll/rest/domain/post/post/dto/PostDto.java
@@ -11,15 +11,15 @@ import java.time.LocalDateTime;
 @Getter
 public class PostDto {
     private long id;
-    private LocalDateTime createDate;
-    private LocalDateTime modifyDate;
+    private LocalDateTime createdDateTime;
+    private LocalDateTime modifiedDateTime;
     private String title;
     private String content;
 
     public PostDto(Post post) {
         this.id = post.getId();
-        this.createDate = post.getCreatedAt();
-        this.modifyDate = post.getModifiedAt();
+        this.createdDateTime = post.getCreatedAt();
+        this.modifiedDateTime = post.getModifiedAt();
         this.title = post.getTitle();
         this.content = post.getContent();
 
