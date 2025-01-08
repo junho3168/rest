c10956d6dad8157e1c888004f001aaca386ec82b
junho3168 <junho3168@naver.com>
2025-01-08 15:05:20 +0900

    MethodArgumentNotValidException 2

--- src/main/java/com/ll/rest/global/globalExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/ll/rest/global/globalExceptionHandler/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.ll.rest.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -26,13 +27,15 @@ public class GlobalExceptionHandler {
     }
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<RsData<Void>> handle (MethodArgumentNotValidException ex){
+        FieldError fieldError = ex.getBindingResult().getFieldError();
+        String field = fieldError.getField();
         String message = ex.getBindingResult().getFieldError().getDefaultMessage();
 
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new RsData<>(
                         "400-1",
-                        message
+                        field + " : " + message
                 ));
     }
 }
