250c6696329af4352cd71a875244eb2165a1bf1c
junho3168 <junho3168@naver.com>
2025-01-08 15:30:04 +0900

    Error message integration

--- src/main/java/com/ll/rest/global/globalExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/ll/rest/global/globalExceptionHandler/GlobalExceptionHandler.java
@@ -8,16 +8,21 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.View;
 
+import java.util.Comparator;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 //밑의 코드는 복사해서 많이 사용한다. (룰을 정하듯이 선언적으로 예외선언 가능)
 
 @ControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
+    private final View error;
+
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<RsData<Void>> handle (NoSuchElementException ex){
+    public ResponseEntity<RsData<Void>> handle(NoSuchElementException ex) {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(new RsData<>(
@@ -25,18 +30,24 @@ public class GlobalExceptionHandler {
                         "해당 데이터가 존재하지 않습니다."
                 ));
     }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<RsData<Void>> handle (MethodArgumentNotValidException ex){
-        FieldError fieldError = ex.getBindingResult().getFieldError();
-        String field = fieldError.getField();
-        String code = fieldError.getCode();
-        String message = ex.getBindingResult().getFieldError().getDefaultMessage();
+    public ResponseEntity<RsData<Void>> handle(MethodArgumentNotValidException ex) {
+        String message = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .filter(error -> error instanceof FieldError)
+                .map(error -> (FieldError) error)
+                .map(error -> error.getField() + "-" + error.getCode() + "-" + error.getDefaultMessage())
+                .sorted(Comparator.comparing(String::toString))
+                .collect(Collectors.joining("\n"));
+
 
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new RsData<>(
-                        "400-" + field + "-" + code,
-                        field + " : " + message
+                        "400-1",
+                        message + " : " + message
                 ));
     }
 }
