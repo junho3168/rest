389c8a0e4f88c282f3caa378b20933d5796d820a
junho3168 <junho3168@naver.com>
2025-01-09 11:08:14 +0900

    RunTimeException

--- src/main/java/com/ll/rest/global/globalExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/ll/rest/global/globalExceptionHandler/GlobalExceptionHandler.java
@@ -59,8 +59,8 @@ public class GlobalExceptionHandler {
                 ));
     }
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<RsData<Void>> handle(IllegalArgumentException ex) {
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<RsData<Void>> handle(RuntimeException ex) {
 
         if (AppConfig.isNotProd()) ex.printStackTrace(); //개발단계에서 오류 출력해줌
 
