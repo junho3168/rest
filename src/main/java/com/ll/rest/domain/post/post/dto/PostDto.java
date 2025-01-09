df2f3bf62c8afc65f377483bb6566e6a781ffa57
junho3168 <junho3168@naver.com>
2025-01-09 12:28:10 +0900

    PostDto has a MemberDto

--- src/main/java/com/ll/rest/domain/post/post/dto/PostDto.java
+++ src/main/java/com/ll/rest/domain/post/post/dto/PostDto.java
@@ -2,6 +2,7 @@ package com.ll.rest.domain.post.post.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ll.rest.domain.member.member.dto.MemberDto;
 import com.ll.rest.domain.post.post.entity.Post;
 import com.ll.rest.global.jpa.entity.BaseTime;
 import jakarta.persistence.Column;
@@ -13,17 +14,24 @@ import java.time.LocalDateTime;
 @Getter
 public class PostDto {
     private long id;
+
     @JsonProperty("createdDatetime")
     private LocalDateTime createDate;
+
     @JsonProperty("modifiedDatetime")
     private LocalDateTime modifyDate;
+
+    private MemberDto author;
+
     private String title;
+
     private String content;
 
     public PostDto(Post post) {
         this.id = post.getId();
         this.createDate = post.getCreatedAt();
         this.modifyDate = post.getModifiedAt();
+        this.author = new MemberDto(post.getAuthor());
         this.title = post.getTitle();
         this.content = post.getContent();
 
